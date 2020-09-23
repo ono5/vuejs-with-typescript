1e593eb99b928fc87c52fd14474b4a058606158f
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-09-24 06:10:57 +0900

    Synchroous tests, asynchrounous vue

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -1,10 +1,26 @@
 import { mount } from '@vue/test-utils'
+import { nextTick } from 'vue'
 import Timeline from './Timeline.vue'
+
 
 describe('Timeline', () => {
     it('renders 3 time periods', () => {
         const wrapper = mount(Timeline)
 
-        const periods = wrapper.findAll('a')
+        expect(wrapper.findAll('[data-test="period"]')).toHaveLength(3)
+    })
+
+    it('udates hte period when clicked', async () => {
+        const wrapper = mount(Timeline)
+        const $today = wrapper.findAll('[data-test="period"]')[0]
+
+        expect($today.classes()).toContain('is-active')
+
+        const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+        await $thisWeek.trigger('click')
+        // await nextTick()
+
+        expect($today.classes()).not.toContain('is-active')
+        expect($thisWeek.classes()).toContain('is-active')
     })
 })
