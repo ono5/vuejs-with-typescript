0c7958908c71469bab2f7c55a31d395327acb48c
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-09-24 10:33:07 +0900

    feat: Defining Post Writer

--- src/NewPost.vue
+++ src/NewPost.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>New Post</div>
+  <post-writer :post="post" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PostWriter from './PostWriter.vue'
+import { Post } from './types'
+import moment from 'moment'
 
 export default defineComponent({
   name: 'NewPost',
+  components: {
+    PostWriter,
+  },
+
+  setup() {
+    const post: Post = {
+      id: -1,
+      title: 'New Post',
+      markdown: "## New Post\nEnter your post here...",
+      html: '',
+      created: moment(),
+      authorId: 0
+    }
+    return {
+      post
+    }
+  }
 })
 </script>
