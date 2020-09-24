6f366fafb2f40a87842729adb3cf37b6f480a5f8
ono5 <kuroneko_head_lord@yahoo.co.jp>
2020-09-24 10:05:51 +0900

    routing with vue router

---
+++ src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <router-view />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
